Refactor decoder and encoder configurations to allow layer-specific normalization, activation, and dropout parameters. Update tabular output projection to use a fixed final activation. Enhance reconstruction loss functions to support masking. Introduce new evaluation and training functions for masked reconstruction in VAE. Improve logging in prediction functions. Adjust validation loss improvement threshold in trainer.

src/neural_blueprints/components/composite/encoder.py
@@ -27,9 +27,9 @@ class Encoder(nn.Module):
         layers = nn.ModuleList()
 
         for layer_config in self.layer_configs:
-            layer_config.normalization = self.normalization
-            layer_config.activation = self.activation
-            layer_config.dropout_p = self.dropout_p
+            layer_config.normalization = self.normalization if layer_config.normalization is None else layer_config.normalization
+            layer_config.activation = self.activation if layer_config.activation is None else layer_config.activation
+            layer_config.dropout_p = self.dropout_p if layer_config.dropout_p is None else layer_config.dropout_p
             layers.append(get_block(layer_config))
 
         layers.append(get_activation(self.final_activation))

src/neural_blueprints/components/composite/projections/output/tabular.py
@@ -49,7 +49,7 @@ class TabularOutputProjection(nn.Module):
                         normalization=normalization,
                         activation=activation,
                         dropout_p=dropout_p,
-                        final_activation="softmax" if cardinality > 1 else "sigmoid"
+                        final_activation="sigmoid"
                     )
                 )
             )

src/neural_blueprints/utils/criterion.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import torch
 import torch.nn.functional as F
 
@@ -99,7 +100,7 @@ def binary_cross_entropy(y_pred, y_true) -> float:
     """
     return F.binary_cross_entropy(y_pred, y_true)
 
-def reconstruction(y_pred: list[torch.Tensor] | torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
+def reconstruction(y_pred: list[torch.Tensor] | torch.Tensor, y_true: torch.Tensor, mask: Optional[torch.Tensor] = None) -> torch.Tensor:
     """
     Computes the mixed-type reconstruction loss for tabular data, combining cross-entropy loss for categorical features and mean squared error (MSE) for continuous features.
 
@@ -116,13 +117,14 @@ def reconstruction(y_pred: list[torch.Tensor] | torch.Tensor, y_true: torch.Tens
         for col_idx in range(len(y_pred)):
             pred = y_pred[col_idx]
             label = y_true[:, col_idx]
+            col_mask = mask[:, col_idx] if mask is not None else torch.ones_like(label, dtype=torch.bool)
 
             if pred.size(1) > 1:
                 # Weighted cross-entropy
-                loss = F.cross_entropy(input = pred, target = label.long(), reduction='sum')
+                loss = F.cross_entropy(input = pred[col_mask], target = label[col_mask].long(), reduction='sum')
             else:
                 # MSE for continuous
-                loss = F.mse_loss(pred.squeeze(-1), label.float(), reduction='sum')
+                loss = F.mse_loss(pred[col_mask].squeeze(-1), label[col_mask].float(), reduction='sum')
             
             total_loss += loss
     elif isinstance(y_pred, torch.Tensor):
@@ -131,7 +133,7 @@ def reconstruction(y_pred: list[torch.Tensor] | torch.Tensor, y_true: torch.Tens
             total_loss = F.mse_loss(y_pred, y_true, reduction='sum')
     else:
         raise ValueError("y_pred must be either a list of tensors for a mixed-type scenario or a single tensor.")
-    return total_loss / len(y_pred)
+    return total_loss / y_true.size(0)
 
 def vae_loss(y_pred: list[torch.Tensor], y_true: torch.Tensor, mu: torch.Tensor, logvar: torch.Tensor) -> torch.Tensor:
     """

src/neural_blueprints/utils/inference/predict.py
@@ -2,6 +2,9 @@ import torch
 import numpy as np
 from ..metrics import accuracy
 
+import logging
+logger = logging.getLogger(__name__)
+
 def get_predict_inference(predicting_type: str):
     if 'reconstruction' in predicting_type:
         return predict_reconstruction
@@ -43,7 +46,7 @@ def predict_reconstruction(
     dis_accuracy = []
     cont_accuracy = []
     for column_idx in range(X.size(1)):
-        print(f"\nFeature Column {column_idx}:")
+        logger.info(f"Feature Column {column_idx}:")
         predicted_attributes = y_pred[column_idx]      # shape: (batch_size, num_classes)
         targets = y[:, column_idx]                     # shape: (batch_size,)
 
@@ -52,16 +55,19 @@ def predict_reconstruction(
         if predicted_attributes.size(1) > 1:
             is_discrete = True
             predicted_attributes = predicted_attributes.softmax(dim=-1).argmax(dim=-1).cpu().numpy()
+            if np.unique(predicted_attributes).size == 1:
+                logger.warning("Warning: Only one class predicted for this feature.")
         else:
             is_discrete = False
             predicted_attributes = predicted_attributes.squeeze(-1).cpu().numpy()
+
         targets = targets[feature_mask].cpu().numpy()
 
-        print("Predicted attribute values:", predicted_attributes[:5])
-        print("True attribute values:", targets[:5])
+        logger.info(f"Predicted attribute values: {predicted_attributes[:5]}")
+        logger.info(f"True attribute values: {targets[:5]}")
 
         accuracy_value = accuracy(torch.tensor(predicted_attributes), torch.tensor(targets))
-        print(f"Accuracy: {accuracy_value:.4f}")
+        logger.info(f"Accuracy: {accuracy_value:.4f}\n")
         if is_discrete:
             dis_accuracy.append(accuracy_value)
         else:
@@ -69,10 +75,10 @@ def predict_reconstruction(
 
     avg_dis_accuracy = np.mean(dis_accuracy)
     avg_cont_accuracy = np.mean(cont_accuracy)
-    print(f"\nAverage Discrete Accuracy: {avg_dis_accuracy:.4f}")
-    print(f"Average Continuous Accuracy: {avg_cont_accuracy:.4f}")
+    logger.info(f"Average Discrete Accuracy: {avg_dis_accuracy:.4f}")
+    logger.info(f"Average Continuous Accuracy: {avg_cont_accuracy:.4f}")
     avg_accuracy = np.mean(dis_accuracy + cont_accuracy)
-    print(f"Overall Average Accuracy: {avg_accuracy:.4f}")
+    logger.info(f"Overall Average Accuracy: {avg_accuracy:.4f}")
 
     return {
         "avg_discrete_accuracy": avg_dis_accuracy,
@@ -99,7 +105,9 @@ def predict_cross_entropy(
         y_pred = y_pred[0]
     
     y_pred = y_pred.argmax(dim=1)
-    print(f"Predictions: {y_pred[:5]}, \n Ground Truth: {y[:5]}")
+    if np.unique(y_pred).size == 1:
+        logger.warning("Warning: Only one class predicted for this feature.")
+    logger.info(f"Predictions: {y_pred[:5]}, \n Ground Truth: {y[:5]}")
     acc = accuracy(y_pred, y)
-    print(f"Prediction Accuracy: {acc:.4f}")
+    logger.info(f"Prediction Accuracy: {acc:.4f}")
     return acc

src/neural_blueprints/utils/inference/train.py
@@ -10,12 +10,22 @@ logger = logging.getLogger(__name__)
 
 def get_train_inference(training_type: str) -> Callable:
     if 'vae' in training_type:
-        if any(loss in training_type for loss in ['mse', 'mae', 'cross_entropy', 'binary_cross_entropy', 'root_mean_squared_error', 'reconstruction']):
+        if  'reconstruction' in training_type:
+            return train_vae_reconstruction
+        elif 'masked_reconstruction' in training_type:
+            return train_vae_masked_reconstruction
+        elif any(loss in training_type for loss in ['mse', 'mae', 'cross_entropy', 'binary_cross_entropy', 'root_mean_squared_error']):
             return train_vae_base
+        else:
+            raise ValueError(f"Unknown VAE training type: {training_type}")
     else:
         if 'nt_xent_loss' in training_type:
             return train_contrastive
-        elif any(loss in training_type for loss in ['mse', 'mae', 'cross_entropy', 'binary_cross_entropy', 'root_mean_squared_error', 'reconstruction']):
+        elif 'masked_reconstruction' in training_type:
+            return train_masked_reconstruction
+        elif 'reconstruction' in training_type:
+            return train_reconstruction
+        elif any(loss in training_type for loss in ['mse', 'mae', 'cross_entropy', 'binary_cross_entropy', 'root_mean_squared_error']):
             return train_base
     
     raise ValueError(f"Unknown training type: {training_type}")
@@ -53,6 +63,92 @@ def train_vae_base(
         optimizer.zero_grad()
         y_pred, mu, logvar = model(X_batch)
         loss = criterion(y_pred = y_pred, y_true = y_batch, mu=mu, logvar=logvar)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item() * X_batch.size(0)
+    avg_loss = total_loss / len(train_loader.dataset)
+    return avg_loss
+
+def train_vae_masked_reconstruction(
+        train_loader: DataLoader,
+        model: nn.Module,
+        optimizer: torch.optim.Optimizer,
+        criterion: Callable
+) -> float:
+    model.train()
+    total_loss = 0.0
+    for batch in train_loader:
+        X_batch = batch[0].to(get_device())
+        y_batch = batch[1].to(get_device())
+        mask = batch[2].to(get_device())
+        optimizer.zero_grad()
+        y_pred, mu, logvar = model(X_batch)
+        loss = criterion(y_pred = y_pred, y_true = y_batch, mu=mu, logvar=logvar, mask=mask)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item() * X_batch.size(0)
+    avg_loss = total_loss / len(train_loader.dataset)
+    return avg_loss
+
+def train_masked_reconstruction(
+        train_loader: DataLoader,
+        model: nn.Module,
+        optimizer: torch.optim.Optimizer,
+        criterion: Callable
+) -> float:
+    model.train()
+    total_loss = 0.0
+    for batch in train_loader:
+        X_batch = batch[0].to(get_device())
+        y_batch = batch[1].to(get_device())
+        mask = batch[2].to(get_device())
+        optimizer.zero_grad()
+        y_pred = model(X_batch)
+        loss = criterion(y_pred = y_pred, y_true = y_batch, mask=mask)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item() * X_batch.size(0)
+    avg_loss = total_loss / len(train_loader.dataset)
+    return avg_loss
+
+def train_vae_reconstruction(
+        train_loader: DataLoader,
+        model: nn.Module,
+        optimizer: torch.optim.Optimizer,
+        criterion: Callable
+) -> float:
+    model.train()
+    total_loss = 0.0
+    for batch in train_loader:
+        if isinstance(batch, list or tuple):
+            X_batch = batch[0].to(get_device())
+        else:
+            X_batch = batch.to(get_device())
+        optimizer.zero_grad()
+        y_pred, mu, logvar = model(X_batch)
+        loss = criterion(y_pred = y_pred, y_true = X_batch, mu=mu, logvar=logvar)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item() * X_batch.size(0)
+    avg_loss = total_loss / len(train_loader.dataset)
+    return avg_loss
+
+def train_reconstruction(
+        train_loader: DataLoader,
+        model: nn.Module,
+        optimizer: torch.optim.Optimizer,
+        criterion: Callable
+) -> float:
+    model.train()
+    total_loss = 0.0
+    for batch in train_loader:
+        if isinstance(batch, list or tuple):
+            X_batch = batch[0].to(get_device())
+        else:
+            X_batch = batch.to(get_device())
+        optimizer.zero_grad()
+        y_pred = model(X_batch)
+        loss = criterion(y_pred = y_pred, y_true = X_batch)
         loss.backward()
         optimizer.step()
         total_loss += loss.item() * X_batch.size(0)

src/neural_blueprints/utils/trainer.py
@@ -75,7 +75,7 @@ class Trainer:
             if self.best_val_loss == float('inf'):
                 self.best_val_loss = val_losses[-1]
                 torch.save(self.model.state_dict(), self.model_path)
-            elif self.best_val_loss - val_losses[-1] >= 1e-4:
+            elif self.best_val_loss - val_losses[-1] >= 1e-3:
                 self.best_val_loss = val_losses[-1]
                 self.epoch_since_improvement = 0
                 torch.save(self.model.state_dict(), self.model_path)